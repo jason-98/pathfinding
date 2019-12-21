1a719d225b7eb696bde610fe4f1dd8a08005bf90
Jason <jasonpilbrough@gmail.com>
2019-12-21 19:08:59 +0200

    add algorithms.js containing Dijkstra's algorithm

---
+++ src/algorithms.js
@@ -0,0 +1,129 @@
+
+class Vertex{
+  constructor(id,isVisitable){
+    this.id = id;
+    this.dist = null;
+    this.prev = null;
+    this.neighbours = [];
+    this.isVisitable = isVisitable
+  }
+
+}
+
+function dijkstra(graph, sourceIndex, targetIndex){
+
+  //determine size of each row in the graph - assumes graph is square grid
+  const rowLength = Math.ceil(Math.sqrt(graph.length))
+
+  //initialise vertex set
+  const vertexSet = []
+  for(var i =0; i < graph.length; i++){
+    let isVisitable
+    if(graph[i]!==3){
+      isVisitable = true
+    }else{
+      isVisitable = false;
+    }
+
+    vertexSet.push(new Vertex(i,isVisitable))
+  }
+
+  //determine all neighbours
+  for(var m = 0; m < vertexSet.length; m++){
+    const rowIndex = Math.floor(m/rowLength) //interpret m as bxb matrix
+    const colIndex = m % rowLength  //interpret m as bxb matrix
+    //loop over neighbouring vertexes (accounting for edge cases), and add to current vertex neighbour list
+    for(var n = Math.max(rowIndex-1,0); n <= Math.min(rowIndex+1, rowLength-1); n++){
+      for(var p = Math.max(colIndex-1,0); p <= Math.min(colIndex+1, rowLength-1); p++){ //TODO handle if not square matrix
+        if(n===rowIndex && p===colIndex){
+          continue; //dont want to add currentVertex as a neighbour to itself
+        }
+        const neighbour = vertexSet[n*rowLength+p]
+
+        //only add to list of neighbours if able to visit
+        if(neighbour.isVisitable){
+            vertexSet[m].neighbours.push(neighbour)
+        }
+
+      }
+    }
+  }
+
+  //initialise array to store all processed vertices
+  var processedVerticies = []
+
+  //set distace to source vertex to 0
+  vertexSet[sourceIndex].dist=0
+
+  while(vertexSet.length>0){
+
+    //represent current vertex index
+    var currentIndex = findIndexOfMinDist(vertexSet)
+    //if cureentIndex === -1 no other vertex is reachable
+    if(currentIndex===-1){
+      break;
+    }
+    //use variable 'u' to represent current vertex index
+    var u = vertexSet.splice(currentIndex,1)[0] //remove vertex from vertexSet and store in u
+
+    //if currentVertex is the target vertex, stop early
+    if(u.id===targetIndex){
+      processedVerticies.push(u)
+      break;
+    }
+
+
+    for(var j = 0; j < u.neighbours.length; j++){
+        const v = u.neighbours[j] //use variable 'v' for current neighbour
+
+        const alt = u.dist +1 // assume all link costs are 1
+        if(v.dist==null || alt < v.dist){
+          v.dist = alt
+          v.prev = u
+        }
+    }
+
+    processedVerticies.push(u)
+
+  }
+
+  //array with indices of vertices on shortest path to target
+  const pathToTarget = []
+  var t = processedVerticies.pop()
+
+  //if target is not found
+  if(t.id!==targetIndex){
+    console.log("No path from source to target")
+    return [];
+  }
+
+  while(t!==null){
+    pathToTarget.unshift(t.id)
+    t = t.prev
+  }
+
+  console.log("Path to target:", pathToTarget)
+  return pathToTarget
+}
+
+
+function findIndexOfMinDist(vertexSet){
+    var minDist = 1000000000; //some large number
+    var indexOfMin = -1 // if -1 is returned, no min was found
+
+    for(var i=0; i < vertexSet.length; i++){
+      const dist = vertexSet[i].dist
+      if(dist===null){
+        continue; //dont consider distance if it is null
+      }
+      if(dist< minDist){
+        minDist = dist
+        indexOfMin = i
+      }
+    }
+    return indexOfMin
+}
+
+
+
+export default dijkstra; // Don’t forget to use export default!
